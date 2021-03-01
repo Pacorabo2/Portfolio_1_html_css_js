bbd85ef2295f54587833555912401daccac77bea
Nicolas Plaza <pacorabo2@hotmail.com>
2021-03-01 12:02:49 +0100

    chore(front/sendMail): get values from form)

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-console.log('Je suis dans le fichier Js');
-
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyC4PqvpliDLPgxZMOXR418BpBtioRiXGpc",
-  authDomain: "test-form-2347e.firebaseapp.com",
-  projectId: "test-form-2347e",
-  storageBucket: "test-form-2347e.appspot.com",
-  messagingSenderId: "472849732922",
-  appId: "1:472849732922:web:c60ca9b15271c0932b2c03"
+  apiKey: "AIzaSyBb4ubzd3BLW6xY0vvrFzOjgbkkFEZb_WA",
+  authDomain: "formulaire-portfolio-b48c7.firebaseapp.com",
+  projectId: "formulaire-portfolio-b48c7",
+  storageBucket: "formulaire-portfolio-b48c7.appspot.com",
+  messagingSenderId: "677576153292",
+  appId: "1:677576153292:web:0159ec83cb1cb2c2a58a69"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Reference contactInfo collections
 let contactInfo = firebase.database().ref("infos");
+
+// Listen for a submit and fire submitForm function
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log("Je suis dans la fonction submitForm");
+
+  // Get input Values
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let message = document.querySelector(".message").value;
+
+  console.log(name, email, message);
+
+  // Save values input form
+  saveContactInfo(name, email, message);
+}
+
+function saveContactInfo(name, email, message) {
+  let newContactInfo = contactInfo.push();
+
+  newContactInfo.set({
+    name: name,
+    email: email,
+    message: message,
+  })
+}
 
 
