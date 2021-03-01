45a3c520d607289e27d2b299cfd0f426cb8afab1
Nicolas Plaza <pacorabo2@hotmail.com>
2021-03-01 08:29:32 +0100

    chore(front/sendMail): connect to firebase, Smtpjs and Firebase config

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-console.log('Hello');
+console.log('Je suis dans le fichier Js');
 
-var accueil = "Accueil";
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC4PqvpliDLPgxZMOXR418BpBtioRiXGpc",
+  authDomain: "test-form-2347e.firebaseapp.com",
+  projectId: "test-form-2347e",
+  storageBucket: "test-form-2347e.appspot.com",
+  messagingSenderId: "472849732922",
+  appId: "1:472849732922:web:c60ca9b15271c0932b2c03"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-console.log(accueil);
+// Reference contactInfo collections
+let contactInfo = firebase.database().ref("infos");
+
+
