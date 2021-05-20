77efa3a48b97ec0ed5e7f3e5d8fd170cecf62f17
Nicolas Plaza <pacorabo2@hotmail.com>
2021-05-20 12:54:04 +0200

    (front/cssAnimations): animtaion of navBar and main section with scss

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var firebaseConfig = {
   apiKey: "AIzaSyBb4ubzd3BLW6xY0vvrFzOjgbkkFEZb_WA",
   authDomain: "formulaire-portfolio-b48c7.firebaseapp.com",
+  databaseURL: "https://formulaire-portfolio-b48c7-default-rtdb.firebaseio.com",
   projectId: "formulaire-portfolio-b48c7",
   storageBucket: "formulaire-portfolio-b48c7.appspot.com",
   messagingSenderId: "677576153292",
@@ -65,19 +66,72 @@ document.querySelector('#phsTeamCard').addEventListener('click', () => {
   }
 });
 
-// // Listener on #chatSite, on click 
-// document.querySelector('#chatSite').addEventListener('click', () => {
-//   if (window.confirm("Une nouvelle fenêtre va s'ouvrir")) {
-//     window.open("https://react-chat-app-porfolio.netlify.app/")
-//   }
-// });
 
-// // Listener on #chatGithub, on click 
-// document.querySelector('#chatGithub').addEventListener('click', () => {
-//   if (window.confirm("Une nouvelle fenêtre va s'ouvrir")) {
-//     window.open("https://github.com/Pacorabo2/React-chat-app-pn")
-//   }
-// });
+// Listener for event on Skills
+const leftAnimTxt = document.querySelector('.skills__skills-text-section')
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 250) {
+    leftAnimTxt.classList.add('leftAnimTxt')
+  } else {
+    leftAnimTxt.classList.remove('leftAnimTxt')
+  }
+})
+
+// Listener for event on Skills Img
+const rigthAnimImg = document.querySelector('.skills__skill-img')
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 250) {
+    rigthAnimImg.classList.add('rigthAnimImg')
+  } else {
+    rigthAnimImg.classList.remove('rigthAnimImg')
+  }
+})
+
+// Listener for event on introduction section
+const introductionSection = document.querySelector('#introduction')
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 700) {
+    introductionSection.classList.add('appear')
+  } else {
+    introductionSection.classList.remove('appear')
+  }
+})
+
+// Listener for event on introduction section
+const serviceTitle = document.querySelector('#services h2, #frontEnd, #backEnd')
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 1900) {
+    serviceTitle.classList.add('appear')
+  } else {
+    serviceTitle.classList.remove('appear')
+  }
+})
+
+// Listener for event on introduction section
+const frontEnd = document.querySelector('#frontEnd')
+const backEnd = document.querySelector('#backEnd')
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 2000) {
+    frontEnd.classList.add('leftAnimTxt')
+    backEnd.classList.add('rigthAnimImg')
+  } else {
+    frontEnd.classList.remove('leftAnimTxt')
+    backEnd.classList.remove('rigthAnimImg')
+  }
+})
+
+// Listener for event on introduction section
+const medias = document.querySelector('#medias')
+const webQuality = document.querySelector('#webQuality')
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 2500) {
+    medias.classList.add('rigthAnimImg')
+    webQuality.classList.add('leftAnimTxt')
+  } else {
+    medias.classList.remove('leftAnimTxt')
+    webQuality.classList.remove('rigthAnimImg')
+  }
+})
 
 function submitForm(e) {
   e.preventDefault();
